51266aaa3fcf8449a94414d7abac1a85e184ee84
Omole Daniel <omoledaniel13@gmail.com>
2022-10-24 18:58:19 +0100

    Setting the stage for the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import {Logo} from "./assets";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className='bg-red-500'>
+        <p className='text-base text-red-800'>This is the beginning of this project!</p>
+
+      </div>
+    );
 }
 
 export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,24 @@
+// Icons
+import bookmark from "./icons/icon-bookmark.svg";
+import check from "./icons/icon-check.svg";
+import closeMenu from "./icons/icon-close-menu.svg";
+import closeModal from "./icons/icon-close-modal.svg";
+import hamburger from "./icons/icon-hamburger.svg";
+
+// Images
+import HeroDesktop from "./images/image-hero-desktop.jpg";
+import HeroMobile from "./images/image-hero-mobile.jpg";
+import LogoMasterCraft from "./images/logo-mastercraft.svg";
+import Logo from "./images/logo.svg";
+
+export {
+    bookmark,
+    check,
+    closeMenu,
+    closeModal,
+    hamburger,
+    HeroDesktop,
+    HeroMobile,
+    LogoMasterCraft,
+    Logo
+}
